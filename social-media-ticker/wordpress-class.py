56686e5cf76887f2ca5fff7363bb1c8b7c4e29e5
Sneha gunta <sgunta@redhat.com>
2019-10-16 13:49:27 -0400

    add function to post a comment on blog

--- social-media-ticker/wordpress-class.py
+++ social-media-ticker/wordpress-class.py
@@ -21,11 +21,37 @@ class Blog:
     def list_comments_on_post(self, post_id, per_page=3):
         return self.get("/comments", post=post_id, per_page=per_page).json()
 
+    def total_comments(self):
+        response = requests.get(self.url + "/comments", params={"per_page": 1})
+        return int(response.headers['X-WP-Total'])
 
-blog = Blog('http://demo.wp-api.org/wp-json/wp/v2')
-posts = blog.list_posts(per_page=5)
+    def comment_on_post(self, post_id, comment_to_post):
+        url = (self.url + '/comments')
+        data = {
+            'post':post_id,
+            'author_name':'Your name',
+            'author_email':'YourEmail@gmail.com',
+            'content':comment_to_post
+        }
+        print("Making a POST request to URL: {}".format(url))
+        response = requests.post(url, data)
+        return response.content
+
+
+
+blog = Blog('http://blog.example.com/wp-json/wp/v2')
+posts = blog.list_posts(per_page=1)
 print([post['title']['rendered'] for post in posts])
 print("😀")
 print(
     *[blog.list_comments_on_post(post_id=post['id'], per_page=5) for post in posts],
+  sep='\n')
+
+print('Enter your comment')
+comment_to_post = input()
+print('Posting your comment')
+print(
+    *[blog.comment_on_post(post_id=post['id'], comment_to_post=comment_to_post) for post in posts],
     sep='\n')
+
+
