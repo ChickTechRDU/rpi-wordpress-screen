0f0d9991160f30dc91a23282d4f2d2d7ba2185c4
Sneha gunta <sgunta@redhat.com>
2019-10-17 17:37:56 -0400

    add programs to poll for posts and comment on posts

--- social-media-ticker/pollingForPosts.py
+++ social-media-ticker/pollingForPosts.py
@@ -1,31 +1,48 @@
-import requests
-import datetime
+from datetime import datetime
 import time
+import requests
 
 class Blog:
 
-   def __init__(self, url):
+    def __init__(self, url):
         self.url = url
 
-   def has_anything_new_been_posted_yet(self):
-       polling = True
-       while polling:
-        response = requests.get(self.url + "/comments", params={"post": 1, "per_page": 1})
-        current_timestamp = datetime.datetime.fromtimestamp(0)
-        print("current timestamp before ",current_timestamp)
-        most_recent_post = datetime.datetime.fromisoformat(response.json()[0]['date'])
-        if most_recent_post > current_timestamp:
-           current_timestamp =  most_recent_post
-           print("a new post just came up on ",self.url, "at ",current_timestamp)
-           polling = False
-        else:
-            polling = True
-        time.sleep(3)
+    def list_latest_posts(self, at_most):
+        response = requests.get(self.url + "/posts", params={"per_page": at_most})
+        return response.json()
 
-blog_urls = {
-    "Alec's blog": "http://demo.wp-api.org/wp-json/wp/v2",
-    "Dustin's blog": "http://demo.wp-api.org/wp-json/wp/v2"
-    }
+    def list_comments_on_post(self, post_id, at_most):
+        response = requests.get(self.url + "/comments", params={"post": post_id, "per_page": at_most})
+        return response.json()
 
-blog = Blog(blog_urls.get("Alec's blog"));
-blog.has_anything_new_been_posted_yet()
+    def total_comments(self):
+        response = requests.get(self.url + "/comments", params={"per_page": 1})
+        return int(response.headers['X-WP-Total'])
+
+    def comment_on_post(self, post_id, comment_to_post):
+        url = (self.url + '/comments')
+        data = {
+            'post': post_id,
+            'author_name': 'Your name',
+            'author_email': 'YourEmail@gmail.com',
+            'content': comment_to_post
+        }
+        print("Making a POST request to URL: {}".format(url))
+        response = requests.post(url, data)
+        return response.content
+
+
+blog = Blog("http://demo.wp-api.org/wp-json/wp/v2")
+last_commented_date = datetime.fromtimestamp(0)
+
+while True:
+    posts = blog.list_latest_posts(at_most=1)
+    if len(posts) > 0:
+        latest_post = posts[0]
+
+        post_date = datetime.fromisoformat(latest_post['date'])
+        if post_date > last_commented_date:
+            last_commented_date = post_date
+            print("a new post just came up on ", blog.url, "at ", last_commented_date)
+            blog.comment_on_post(latest_post['id'])
+    time.sleep(3)
